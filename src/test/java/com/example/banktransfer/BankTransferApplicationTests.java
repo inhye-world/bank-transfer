127a1e3b4fb824498f84ba02f8788ebe8e805208
happytoseeyou <happytoseeyou@mealgenui-MacBookPro-3.local>
2022-05-12 15:46:11 +0900

    Initial commit

---
+++ src/test/java/com/example/banktransfer/BankTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banktransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
