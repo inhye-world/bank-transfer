2160daf990e0cf2ae82e97236eea991656ed0dec
ksy <ksy990628@ewhain.net>
2022-05-17 11:23:14 +0900

    chore: Merge checkAccountName feature

--- src/main/java/com/example/banktransfer/controller/AccountInfoController.java
+++ src/main/java/com/example/banktransfer/controller/AccountInfoController.java
@@ -1,8 +1,6 @@
 package com.example.banktransfer.controller;
 import com.example.banktransfer.dto.BankRequestDto;
 import com.example.banktransfer.service.AccountInfoService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1")
--- src/main/java/com/example/banktransfer/service/AccountInfoService.java
+++ src/main/java/com/example/banktransfer/service/AccountInfoService.java
@@ -1,29 +1,99 @@
 package com.example.banktransfer.service;
-
 import com.example.banktransfer.dto.AccountRequestDto;
 import com.example.banktransfer.dto.BankRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import java.io.*;
+import javax.net.ssl.HttpsURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Random;
+import com.fasterxml.jackson.databind.JsonNode;
 
+@Slf4j
 @Service
 public class AccountInfoService {
-    public static BankRequestDto getAccountInfo(BankRequestDto bankRequestDto) {
-        AccountRequestDto accountRequestDto = AccountRequestDto.builder()
-                .BANK_CD("088")
-                .SEARCH_ACCT_NO("110486040548")
-                .ACNM_NO("990628")
-                .ICHE_AMT("100")
-                .TRSC_SEQ_NO("7580200")
-                .build();
-        bankRequestDto.getREQ_DATA().add(accountRequestDto);
+    public static Boolean getAccountName(BankRequestDto bankRequestDto) {
 
+        String url = "https://dev2.coocon.co.kr:8443/sol/gateway/acctnm_rcms_wapi.jsp?JSONData=";
 
-        BankRequestDto dto =  BankRequestDto.builder()
-                .SECR_KEY(bankRequestDto.getSECR_KEY())
-                .KEY(bankRequestDto.getKEY())
-                .CHAR_SET(bankRequestDto.getKEY())
-                .REQ_DATA(bankRequestDto.getREQ_DATA())
-                .build();
+        String key = bankRequestDto.getKEY();
+        String secr_key = bankRequestDto.getSECR_KEY();
+        String char_set = bankRequestDto.getCHAR_SET();
+        List<AccountRequestDto> dto2 = bankRequestDto.getREQ_DATA();
+        String bank_cd = dto2.get(0).getBANK_CD();
+        String search_acct_no = dto2.get(0).getSEARCH_ACCT_NO();
+        String acnm_no = dto2.get(0).getACNM_NO();
+        String iche_amt = dto2.get(0).getICHE_AMT();
+        String trsc_seq_no = randomCode(7);
+        String name = dto2.get(0).getNAME();
+        String req = "{\"SECR_KEY\":\""+secr_key+"\",\"KEY\":\""+key+"\"," +
+                "\"DOMN\":\"https://dev2.coocon.co.kr:8443/sample_acctnm_rcms_kib.jsp\",\"TRG\":\"\",\"SORT\":\"\"," + "\"PG_PER_CNT\":\"\",\"PG_NO\":\"\"," +
+                "\"REQ_DATA\":[{\"BANK_CD\":\""+bank_cd+"\"," +
+                "\"SEARCH_ACCT_NO\":\""+search_acct_no+"\",\"ACNM_NO\":\""+acnm_no+"\"," +
+                "\"ICHE_AMT\":\""+iche_amt+"\",\"TRSC_SEQ_NO\":\""+trsc_seq_no+"\"}]}\n";
 
-        return dto;
+        String domain = url + req;
+        String accountName = getAccountInfo(domain);
+        if (name.equals(accountName)){
+            log.info("계좌 조회에 성공했습니다.");
+            return true;
+        }
+        log.info("계좌 조회에 실패했습니다. 계좌 정보를 다시 확인해주세요.");
+        return false;
+    }
+
+    public static String getAccountInfo(String domain){
+        try {
+            URL url = new URL(domain);
+            HttpsURLConnection http = (HttpsURLConnection) url.openConnection();
+            http.setRequestMethod("GET");
+            http.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+            System.out.println(http.getResponseCode()+" "+http.getResponseMessage());
+
+            JsonNode jsonNode = getReturnNode(http);
+            System.out.println(jsonNode);
+
+            String code = jsonNode.get("RSLT_CD").toString().substring(1,jsonNode.get("RSLT_CD").toString().length()-1);
+            String completed = "000";
+
+            if (completed.equals(code)){
+                JsonNode jsonNode1 = jsonNode.get("RESP_DATA").get(0);
+                return jsonNode1.get("ACCT_NM").toString().substring(1,jsonNode1.get("ACCT_NM").toString().length()-1);
+            }
+        } catch (MalformedURLException e) {
+            System.out.println("MalformedURLException");
+        } catch (IOException e) {
+        }
+        return null;
+    }
+
+    public static JsonNode getReturnNode( HttpsURLConnection connection ) throws IOException {
+        BufferedReader in = new BufferedReader( new InputStreamReader(
+                connection.getInputStream(), "EUC-KR" ) );
+        StringBuffer buffer = new StringBuffer();
+        String decodedString;
+        while( ( decodedString = in.readLine() ) != null ) {
+            buffer.append( decodedString );
+        }
+        in.close();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(buffer.toString());
+        return jsonNode;
+    }
+
+    public static String randomCode(int length){
+        String rdStr = "";
+
+        StringBuffer sb = new StringBuffer();
+        Random rn = new Random();
+
+        for(int i = 0; i < length; i++){
+            sb.append(rn.nextInt(10));
+            rdStr = sb.toString();
+        }
+        return rdStr;
     }
 }
